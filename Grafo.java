c6c396cb9d498e2aad91b477aa90b7028dc6829a
Joao Guilherme <lecostela123@gmail.com>
2024-11-19 14:46:59 -0300

    criação da classe grafo

---
+++ Grafo.java
@@ -0,0 +1,114 @@
+// Java program to implement Graph
+// with the help of Generics
+
+import java.util.*;
+
+class Graph<T> {
+    private Map<T, List<T> > map = new HashMap<>();
+
+    public void addVertex(T s)
+    {
+        map.put(s, new LinkedList<T>());
+        
+
+    }
+
+    public void addEdge(T source, T destination,
+                        boolean bidirectional)
+    {
+
+        if (!map.containsKey(source))
+            addVertex(source);
+
+        if (!map.containsKey(destination))
+            addVertex(destination);
+
+        map.get(source).add(destination);
+        if (bidirectional == true) {
+            map.get(destination).add(source);
+        }
+    }
+
+    // This function gives the count of vertices
+    public void getVertexCount()
+    {
+        System.out.println("The graph has "
+                           + map.keySet().size()
+                           + " vertex");
+    }
+
+    // This function gives the count of edges
+    public void getEdgesCount(boolean bidirection)
+    {
+        int count = 0;
+        for (T v : map.keySet()) {
+            count += map.get(v).size();
+        }
+        if (bidirection == true) {
+            count = count / 2;
+        }
+        System.out.println("The graph has " + count
+                           + " edges.");
+    }
+
+    
+
+    // This function gives whether an edge is present or
+    // not.
+    public void hasEdge(T s, T d)
+    {
+        if (map.get(s).contains(d)) {
+            System.out.println(
+                "The graph has an edge between " + s
+                + " and " + d + ".");
+        }
+        else {
+            System.out.println(
+                "The graph has no edge between " + s
+                + " and " + d + ".");
+        }
+    }    
+
+    // Prints the adjancency list of each vertex.
+    @Override public String toString()
+    {
+        StringBuilder builder = new StringBuilder();
+
+        for (T v : map.keySet()) {
+            builder.append(v.toString() + ": ");
+            for (T w : map.get(v)) {
+                builder.append(w.toString() + " ");
+            }
+            builder.append("\n");
+        }
+
+        return (builder.toString());
+    }
+}
+
+// Driver Code
+public class Main {
+
+    public static void main(String args[])
+    {
+
+        // Object of graph is created.
+        Graph<Integer> g = new Graph<Integer>();
+
+        // edges are added.
+        // Since the graph is bidirectional,
+        // so boolean bidirectional is passed as true.
+        g.addEdge(0, 1, true);
+        g.addEdge(0, 4, true);
+        g.addEdge(1, 2, true);
+        g.addEdge(1, 3, true);
+        g.addEdge(1, 4, true);
+        g.addEdge(2, 3, true);
+        g.addEdge(3, 4, true);
+
+        // Printing the graph
+        System.out.println("Graph:\n" + g.toString());
+
+
+    }
+}
